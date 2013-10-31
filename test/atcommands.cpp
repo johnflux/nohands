db6d4b7a9daf3bfaaf665e741715483263fca7c4
John Tapsell <john.tapsell.ext@basyskom.com>
2013-10-31 17:16:35 +0000

    Start work on atcommands unit test

---
+++ test/atcommands.cpp
@@ -0,0 +1,41 @@
+/*
+ * Software Bluetooth Hands-Free Implementation
+ *
+ * Copyright (C) 2008 Sam Revitch <samr7@cs.washington.edu>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/*
+ * Unit test for SoundIoPump
+ * Currently this test is very weak and doesn't catch all the
+ * types of problems it should be catching.
+ */
+
+#include <stdio.h>
+#include <assert.h>
+
+#include <libhfp/soundio.h>
+#include <libhfp/soundio-buf.h>
+#include <libhfp/events-indep.h>
+
+using namespace libhfp;
+
+int
+main(int argc, char **argv)
+{
+
+	return 0;
+}
